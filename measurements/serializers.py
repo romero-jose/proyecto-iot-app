897599a66f45d146cc4138c3e76ca1c199c4575a
romero-jose <jose.romero@ing.uchile.cl>
2021-11-29 18:10:46 -0300

    Makes the API use ids instead of hyperlinks

--- measurements/serializers.py
+++ measurements/serializers.py
@@ -4,13 +4,13 @@ from rest_framework import serializers
 from measurements.models import Measurement, Sensor
 
 
-class SensorSerializer(serializers.HyperlinkedModelSerializer):
+class SensorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Sensor
         fields = ['name']
 
 
-class MeasurementSerializer(serializers.HyperlinkedModelSerializer):
+class MeasurementSerializer(serializers.ModelSerializer):
     class Meta:
         model = Measurement
         fields = ['sensor', 'measurement', 'date']
